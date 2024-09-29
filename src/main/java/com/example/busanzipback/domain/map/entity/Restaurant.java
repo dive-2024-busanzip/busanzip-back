76900882e5407f8f3f973b26c1239fe4eb105cb7
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2024-09-29 20:27:57 +0900

    [Feat] 특정 위치 주변 식당 리스트 조회 구현
    * feat: Restaurant entity 생성
    
    * feat: 주변 식당 리스트 조회 구현

---
+++ src/main/java/com/example/busanzipback/domain/map/entity/Restaurant.java
@@ -0,0 +1,60 @@
+package com.example.busanzipback.domain.map.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Restaurant {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "restaurant_id")
+	private Long id;
+	@Column(nullable = false)
+	private String restaurantName;
+	@Column(nullable = false)
+	private String district;
+	@Column(columnDefinition = "decimal(10, 6)", nullable = false)
+	private Double latitude;
+	@Column(columnDefinition = "decimal(10, 6)", nullable = false)
+	private Double longitude;
+	@Column(nullable = false)
+	private String address;
+	private String addressExtra;
+	private String contact;
+	private String homepage;
+	private String operatingHours;
+	private String mainMenu;
+	private String imageUrl;
+	private String thumbnailImageUrl;
+	private String details;
+
+	@Builder
+	public Restaurant(String restaurantName, String district, Double latitude, Double longitude,
+		String address,
+		String addressExtra, String contact, String homepage, String operatingHours, String mainMenu, String imageUrl,
+		String thumbnailImageUrl, String details) {
+		this.restaurantName = restaurantName;
+		this.district = district;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.address = address;
+		this.addressExtra = addressExtra;
+		this.contact = contact;
+		this.homepage = homepage;
+		this.operatingHours = operatingHours;
+		this.mainMenu = mainMenu;
+		this.imageUrl = imageUrl;
+		this.thumbnailImageUrl = thumbnailImageUrl;
+		this.details = details;
+	}
+}
