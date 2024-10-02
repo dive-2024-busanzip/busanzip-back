4f6c360f94d6f30849240de3dd51237191ceafcf
Gyulguma <ybg653924@gmail.com>
2024-10-02 22:31:13 +0900

    hotfix: test 설정

--- src/test/java/com/example/busanzipback/BusanzipBackApplicationTests.java
+++ src/test/java/com/example/busanzipback/BusanzipBackApplicationTests.java
@@ -2,8 +2,11 @@ package com.example.busanzipback;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BusanzipBackApplicationTests {
 
 	@Test
