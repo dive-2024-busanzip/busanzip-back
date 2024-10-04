731be9bfa936d099421527aff5dc64f8d990d3b1
hongkikii <hyangmeeh@icloud.com>
2024-10-04 21:23:06 +0900

    chore: RestTemplate 설정 추가

--- src/main/java/com/example/busanzipback/BusanzipBackApplication.java
+++ src/main/java/com/example/busanzipback/BusanzipBackApplication.java
@@ -2,12 +2,19 @@ package com.example.busanzipback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BusanzipBackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BusanzipBackApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
