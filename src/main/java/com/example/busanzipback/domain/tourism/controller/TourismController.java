7b10862aa2bc0a08f2fb4658d18f5ce7424599bf
hongkikii <hyangmeeh@icloud.com>
2024-10-04 23:19:45 +0900

    fix: http method 어노테이션 추가

--- src/main/java/com/example/busanzipback/domain/tourism/controller/TourismController.java
+++ src/main/java/com/example/busanzipback/domain/tourism/controller/TourismController.java
@@ -5,6 +5,7 @@ import com.example.busanzipback.domain.tourism.dto.TourismResponse;
 import com.example.busanzipback.domain.tourism.service.TourismService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class TourismController {
 
     private final TourismService tourismService;
 
+    @PostMapping
     public ResponseEntity<TourismResponse> create(@RequestBody TourismRequest request) {
         return ResponseEntity.ok(tourismService.create(request));
     }
